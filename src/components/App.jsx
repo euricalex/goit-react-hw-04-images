99b41dd336f6feec6451454398009d23d09b94c4
Alexander <122707746+euricalex@users.noreply.github.com>
2023-09-15 03:36:29 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar/SearchBar';
 import * as API from './api';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -14,40 +14,19 @@ export function App() {
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [totalPages, setTotalPages] = useState(0);
-  const [debouncedSearchName, setDebouncedSearchName] = useState('');
-
-  // Используем useCallback для создания мемоизированной функции debounce
-  const debouncedSearch = useCallback(
-    debounce((query) => {
-      setDebouncedSearchName(query);
-    }, 1000),
-    []
-  );
 
   useEffect(() => {
     if (searchName.trim() === '') {
-      if (isLoading) {
-        toast.info('Enter a search query.', {
-          position: toast.POSITION.TOP_RIGHT,
-        });
-      }
       return;
     }
 
-    setCurrentPage(1); // Обнуляем currentPage при новом поиске
+    setCurrentPage(1);
     setTotalPages(0);
     setIsLoading(true);
 
-    // Обновляем debouncedSearchName с задержкой
-    debouncedSearch(searchName);
-  }, [searchName, debouncedSearch, isLoading]);
-
-  useEffect(() => {
-    if (debouncedSearchName === '' || currentPage === 0) return;
-
     const loadImages = async () => {
       try {
-        const data = await API.getImages(debouncedSearchName, currentPage);
+        const data = await API.getImages(searchName, currentPage);
 
         if (data.hits.length === 0) {
           toast.dismiss();
@@ -69,12 +48,26 @@ export function App() {
     };
 
     loadImages();
-  }, [debouncedSearchName, currentPage]);
+  }, [searchName, currentPage]);
+
+  const handleSubmit = (query) => {
+    if (query.trim() === '') {
+      toast.info('Enter a search query.', {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      return;
+    }
+
+    setSearchName(`${Date.now()}/${query}`);
+    setImages([]);
+    setCurrentPage(1);
+  };
 
   const resetGallery = () => {
     setImages([]);
     setTotalPages(0);
-    setCurrentPage(1); // Добавляем сброс currentPage при сбросе галереи
+    setCurrentPage(1);
+    setSearchName('');
   };
 
   const loadMore = () => {
@@ -84,7 +77,7 @@ export function App() {
   return (
     <AppWrapper>
       <ToastContainer transition={Slide} />
-      <SearchBar onSubmit={debouncedSearch} onReset={resetGallery} />
+      <SearchBar onSubmit={handleSubmit} onReset={resetGallery} />
       {images.length > 0 && <ImageGallery images={images} />}
 
       {isLoading && <Loader />}
@@ -96,12 +89,3 @@ export function App() {
 }
 
 export default App;
-
-// Функция debounce для задержки вызова функций
-function debounce(func, wait) {
-  let timeout;
-  return function (...args) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, args), wait);
-  };
-}
